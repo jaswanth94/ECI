18ec4a35a53752976d7ec872d92b85d7e4df7991
jaswanth valupadasu <49828947+jaswanth94@users.noreply.github.com>
2019-04-29 13:41:59 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php include "templates/header.php"; ?>
+
+<ul>
+  <li>
+    <a href="registerVoter.php"><strong>New Voter</strong></a>
+  </li>
+  <li>
+    <a href="searchVoter.php"><strong>Search Voter</strong></a>
+  </li>
+</ul>
+
+<?php include "templates/footer.php"; ?>
