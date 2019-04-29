cb8537e039c9aa0d491caae4e7dc8e9d4dd8c6e6
jaswanth valupadasu <49828947+jaswanth94@users.noreply.github.com>
2019-04-29 13:44:25 +0530

    Create header.php

---
+++ header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="x-ua-compatible" content="ie=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <title>Election Commission of India</title>
+
+    <link rel="stylesheet"  type="text/css" href="css/style.css" />
+<style>
+body {
+  background-color: linen;
+}
+
+h1 {
+  color: black;
+}
+</style>
+  </head>
+
+  <body>
+        <img src="https://i.imgur.com/9toFGlr.jpg" alt="Election Commission of India" style="width:1700px;height:160px;">
+  </body>
+</html>
