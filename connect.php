ecb9cfebb8f892a7bff463bb207d4731ec5b4bbb
jaswanth valupadasu <49828947+jaswanth94@users.noreply.github.com>
2019-04-29 13:42:57 +0530

    Create connect.php

---
+++ connect.php
@@ -0,0 +1,10 @@
+<?php
+$host = "localhost";
+$username = "id9436629_voterinfo";
+$password = "!sru1994";
+$dbname = "id9436629_voterinfo";
+$dsn        = "mysql:host=$host;dbname=$dbname"; // will use later
+$options    = array(
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+              );
+?>
