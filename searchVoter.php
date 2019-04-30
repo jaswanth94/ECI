0a608d9d7625fccd8c38038b565d7b3cb86e0460
jaswanth valupadasu <49828947+jaswanth94@users.noreply.github.com>
2019-04-30 16:16:33 +0530

    Update searchVoter.php

--- searchVoter.php
+++ searchVoter.php
@@ -9,18 +9,16 @@ if (isset($_POST['submit'])) {
       $connection = new PDO($dsn, $username, $password, $options);
     $sql = "SELECT *
 FROM VoterDetails
-WHERE firstname=:firstname && dob=:dob && gender=:gender && district=:district && state=:state";
+WHERE firstname=:firstname && lastname=:lastname && dob=:dob && district=:district";
 $firstname   = $_POST['firstname'];
+$lastname    = $_POST['lastname'];
 $dob         = $_POST['dob'];
-$gender      = $_POST['gender'];
 $district    = $_POST['district'];
-$state       = $_POST['state'];
 $statement = $connection->prepare($sql);
 $statement->bindParam(':firstname', $firstname, PDO::PARAM_STR);
+$statement->bindParam(':lastname', $lastname, PDO::PARAM_STR);
 $statement->bindParam(':dob', $dob, PDO::PARAM_STR);
-$statement->bindParam(':gender', $gender, PDO::PARAM_STR);
 $statement->bindParam(':district', $district, PDO::PARAM_STR);
-$statement->bindParam(':state', $state, PDO::PARAM_STR);
 $statement->execute();
 
 $result = $statement->fetchAll();
@@ -29,16 +27,92 @@ $result = $statement->fetchAll();
   }
 }
 ?>
-<?php require  "header.php"; ?>
+<?php require  "templates/header.php"; ?>
+
+<!DOCTYPE html>
+<html>
+<style>
+body {
+  font-family: Arial;
+}
+input[type=text], select {
+  width: 70%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: block;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+}
+input[type=submit] {
+  width: 70%;
+  background-color: #4CAF50;
+  color: white;
+  padding: 14px 20px;
+  margin: 8px 0;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+div.container {
+  border-radius: 5px;
+  background-color: #f2f2f2;
+  padding: 20px;
+}
+
+input[type=submit]:hover {
+  background-color: #45a049;
+</style>
+<body>
+<div class="container">
+<form method="post">
+    	<label for="firstname">First Name : </label>
+    	<input type="text" id="firstname" name="firstname"><br>
+    	<label for="lastname">Last Name : </label>
+    	<input type="text" id="lastname" name="lastname"><br>
+    	<label for="dob">Date of Birth : </label>
+    	<input type="text" id="dob" name="dob"><br>
+        <label for="district">District : </label>
+        <input type="text" id="district" name="district"><br>
+        <input type="submit" name="submit" value="Search">
+    </form>
+<a href="index.php">Back to home</a>
+</body>
+</div>
+</html>
+
 <?php
 if (isset($_POST['submit'])) {
   if ($result && $statement->rowCount() > 0) { ?>
     <h2>Results</h2>
 
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+table {
+  border-collapse: collapse;
+  width: 100%;
+}
+th {
+  height: 35px;
+}
+th, td {
+  padding: 8px;
+  text-align: left;
+}
+table, td, th {
+  border-bottom: 2px solid black;
+}
+tr:nth-child(even) {background-color: #f2f2f2;}
+</style>
+</head>
+<body>
+<div style="overflow-x:auto;">   
     <table>
       <thead>
 <tr>
-  <th>#</th>
+  <th>Voter ID</th>
   <th>First Name</th>
   <th>Last Name</th>
   <th>Date Of Birth</th>
@@ -49,6 +123,7 @@ if (isset($_POST['submit'])) {
   <th>Pin Code</th>
   <th>District</th>
   <th>State</th>
+  <th> </th>
 </tr>
       </thead>
       <tbody>
@@ -65,6 +140,7 @@ if (isset($_POST['submit'])) {
 <td><?php echo $row["pc"]; ?> </td>
 <td><?php echo $row["district"]; ?> </td>
 <td><?php echo $row["state"]; ?> </td>
+<td><input type="submit" name="submit" value="Print Voter ID"></td>
 
       </tr>
     <?php } ?>
@@ -74,21 +150,8 @@ if (isset($_POST['submit'])) {
     No results found for <?php echo $_POST['firstname']; ?>
   <?php }
 } ?>
-<form method="post">
-    	<label for="firstname">First Name : </label>
-    	<input type="text" id="firstname" name="firstname"><br>
-    	<label for="lastname">Last Name : </label>
-    	<input type="text" id="lastname" name="lastname"><br>
-    	<label for="dob">Date of Birth : </label>
-    	<input type="text" id="dob" name="dob"><br>
-        <label for="gender">Gender : </label>
-    	<input type="text" id="gender" name="gender"><br>
-    	<label for="district">District : </label>
-        <input type="text" id="district" name="district"><br>
-        <label for="state">State : </label>
-        <input type="text" id="state" name="state"><br>
-        <input type="submit" name="submit" value="Search">
-    </form>
-<a href="index.php">Back to home</a>
+</div>
+</body>
+</html>
 
-    <?php include "footer.php"; ?>
+    <?php include "templates/footer.php"; ?>
