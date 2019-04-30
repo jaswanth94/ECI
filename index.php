c00b31c2cbe59293a2a44b443a3b1d75170b282c
jaswanth valupadasu <49828947+jaswanth94@users.noreply.github.com>
2019-04-30 16:17:21 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,24 @@
-<?php include "header.php"; ?>
+<?php include "templates/header.php"; ?>
 
+<!DOCTYPE html>
+<html>
+<style>
+body {
+  font-family: Arial;
+  
+div.random {
+color: black
+}
+</style>
+<body>
+<div class="random">
 <ul>
-  <li>
-    <a href="registerVoter.php"><strong>New Voter</strong></a>
-  </li>
-  <li>
-    <a href="searchVoter.php"><strong>Search Voter</strong></a>
-  </li>
+        <a href="registerVoter.php" style="font-size: 50px; color: black;"><strong>New Voter</strong></a><br>
+        <a>         </a><br>
+      <a href="searchVoter.php" style="font-size: 50px;color: black;"><strong>Search Voter</strong></a>
 </ul>
-
-<?php include "footer.php"; ?>
+<img src="https://i.imgur.com/anllMg7.jpg" alt="Hon'ble Commission" style="width:300px;height:700px;padding-left: 1000px;padding-top: 10px;">
+</div>
+</body>
+</html>
+<?php include "templates/footer.php"; ?>
